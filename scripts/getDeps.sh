4c1857b0ba57ef1ac21cc56576632435c3da3b68
Alessandro Pasotti <elpaso@itopen.it>
2018-11-12 10:08:48 +0100

    Add expect to apt-get install

--- scripts/getDeps.sh
+++ scripts/getDeps.sh
@@ -7,7 +7,7 @@ pip3 install -r /usr/local/requirements.txt
 # Add OpenCL develompent files and supervisor
 LC_ALL=C DEBIAN_FRONTEND=noninteractive  \
     apt-get install -y opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev \
-    supervisor
+    supervisor expect
 
 
 #chmod -R a+w /usr/lib/x86_64-linux-gnu/qt5/plugins/designer/
